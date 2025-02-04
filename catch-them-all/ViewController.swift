7a3383d5149d3949167ca71cde9451b23bed9bd1
Anisovich_Maria <anisovich.masha@gmail.com>
2025-02-04 10:26:12 +0100

    Initial Commit

---
+++ catch-them-all/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  catch-them-all
+//
+//  Created by Мария Анисович on 04.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
